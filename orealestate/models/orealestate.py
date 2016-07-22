3b9f4a42c870b1664c94932e2e98447208b12b14
avw-odoo <alainvandewerve@gmail.com>
2016-07-22 11:24:21 +0200

    add property to activity analysis report

--- orealestate/models/orealestate.py
+++ orealestate/models/orealestate.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 from openerp import api, fields, models
+from openerp import tools
 
 
 
@@ -13,6 +14,47 @@ class Lead(models.Model):
     mandate = fields.Boolean('Is a mandate', readonly=True)
     property_account_manager = fields.Many2one(related='property_id.user_id', string='Account manager')
 
+
+
+
+class crm_activity_report(models.Model):
+    """ CRM Lead Analysis """
+    _inherit = "crm.activity.report"
+
+
+    property_id = fields.Many2one('orealestate.realestate', string='Property', help='Linked property.')
+
+
+    def init(self, cr):
+        tools.drop_view_if_exists(cr, 'crm_activity_report')
+        cr.execute("""
+            CREATE OR REPLACE VIEW crm_activity_report AS (
+                select
+                    m.id,
+                    m.subtype_id,
+                    m.author_id,
+                    m.date,
+                    m.subject,
+                    l.id as lead_id,
+                    l.property_id,
+                    l.user_id,
+                    l.team_id,
+                    l.country_id,
+                    l.company_id,
+                    l.stage_id,
+                    l.partner_id,
+                    l.type as lead_type,
+                    l.active,
+                    l.probability
+                from
+                    "mail_message" m
+                join
+                    "crm_lead" l
+                on
+                    (m.res_id = l.id)
+                WHERE
+                    (m.model = 'crm.lead')
+            )""")
 
 
 
