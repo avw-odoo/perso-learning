9ed763bc92ecf393b48ed1679d9cf83a6cc8ffcc
Alain van de Werve <avw@odoo.com>
2018-03-15 17:12:39 +0100

    remove old directory

--- Old/gestimmodoo_kanban/__openerp__.py
+++
@@ -1,5 +0,0 @@
- {'name': 'Gestimmodoo Kanban',
-    'description': 'Kanban board for Gestimmodoo properties.',
-    'author': 'Alain vdw',
-    'depends': ['gestimmodoo'],
-    'data': ['views.xml'] }
--- Old/gestimmodoo_kanban/models.py
+++
@@ -1,6 +0,0 @@
-from openerp import models, fields
-
-class gestimmodoo(models.Model):
-  _inherit = 'gestimmodoo.gestimmodoo'
-  priority = fields.Selection([('0', 'Low'), ('1', 'Normal'), ('2', 'High')],'Priority', default='0')
-  kanban_state = fields.Selection([('normal', 'In Progress'),('blocked', 'Blocked'),('done', 'Ready for next stage')],'Kanban State', default='normal')
